b3701ae1db4b19dca2f9d5f479731ebcd03fc8ed
Angel75Git <aduboncenteno@usf.edu>
2025-03-30 19:11:01 -0400

    Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -19,5 +19,38 @@ function fetchProductsThen() {
 }
 //calling function
 fetchProductsThen();
+//Task 3 
+//Getting product div
+const mainContainer = document.getElementById('product-container')
 
 //Different way to fetch products
+
+//declaring async
+async function fetchProductsAsync() {
+    try {
+        const response = await fetch(Base_URL);
+        if (!response.ok){
+            throw new Error(`Failed to Load Products ${response.status}`);
+        }
+        const newProduct = await response.json() //Convert response to Json
+        displayProducts(newProduct)
+
+    } catch (error) {
+        handleError(error);
+    }
+}
+//Helper function to display products to page
+function displayProducts(newProduct) {
+    //looping = creating divs to store new products
+    newProduct.forEach(product => {
+        const div = document.createElement('div');
+        div.innerHTML = `<h3>${product.fields.name}</h3>`;
+        mainContainer.appendChild(div);
+    });    
+
+}//Handling Errors displays red message
+function handleError(error) {
+    mainContainer.innerHTML = `<p style="color:red;">Failed to load products</p>`;
+}
+
+fetchProductsAsync()
